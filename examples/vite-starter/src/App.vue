723a06e61b31f7d2fbb3a7a740868031527629fe
Prakash <qxprakash@gmail.com>
2025-05-26 00:50:25 +0530

    bug reproduced on safari

---
+++ examples/vite-starter/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Dashboard } from '@uppy/vue';
+import Uppy from "@uppy/core"
+import GoldenRetriever from "@uppy/golden-retriever"
+
+import "@uppy/core/dist/style.css"
+import "@uppy/dashboard/dist/style.css"
+
+const uppy = new Uppy().use(GoldenRetriever, { serviceWorker: true })
+</script>
+
+<template>
+  <Dashboard :uppy="uppy" />
+</template>
